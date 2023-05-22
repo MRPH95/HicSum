d880c4107664197e7b2f9570ae84b1fa48d62084
MRPH95 <134096222+MRPH95@users.noreply.github.com>
2023-05-21 20:07:51 -0400

    Update SLherothreejs.js

--- SLherothreejs.js
+++ SLherothreejs.js
@@ -10,7 +10,7 @@ function init() {
     0.1,
     1000
   );
-  camera.position.set(0, 0, 2); // Adjust the camera position as needed
+  camera.position.set(0, 2, 2); // Adjust the camera position as needed
 
   // Create a WebGL renderer
   var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
