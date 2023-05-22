9b2c0370819406a27c21bd5bb0ec21832f81ae67
MRPH95 <134096222+MRPH95@users.noreply.github.com>
2023-05-21 20:10:44 -0400

    Update SLherothreejs.js

--- SLherothreejs.js
+++ SLherothreejs.js
@@ -49,7 +49,7 @@ function init() {
       var model = gltf.scene;
 
       // Set the initial position, scale, and rotation of the model as desired
-      model.position.set(0, -2, -1);
+      model.position.set(0, -.2, -1);
       model.scale.set(.1, .1, .1);
       model.rotation.set(0, 0, 0);
 
