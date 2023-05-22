58e323c2d7fc05bae244cc9709ec24d580ba41c3
MRPH95 <134096222+MRPH95@users.noreply.github.com>
2023-05-22 10:44:44 -0400

    Update SLherothreejs.js

--- SLherothreejs.js
+++ SLherothreejs.js
@@ -145,13 +145,38 @@ function init() {
         scrollY = window.scrollY;
       });
 
+// Constants
+var MOUSE_SENSITIVITY = 0.01; // Adjust the mouse sensitivity as needed
+var SCROLL_SENSITIVITY = 0.01; // Adjust the scroll sensitivity as needed
+
+// Variables
+var targetRotationY = 0;
+var targetCameraY = 0;
+
+// Event listeners
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+document.addEventListener('wheel', onDocumentMouseWheel, false);
+
+// Mouse move event handler
+function onDocumentMouseMove(event) {
+  var mouseX = (event.clientX - window.innerWidth / 2) * MOUSE_SENSITIVITY;
+  targetRotationY = mouseX;
+}
+
+// Mouse wheel event handler
+function onDocumentMouseWheel(event) {
+  targetCameraY += event.deltaY * SCROLL_SENSITIVITY;
+}
+
 // Animation loop
 function animate() {
   requestAnimationFrame(animate);
 
-  // Move the camera in the y-direction based on scroll position
-  var cameraY = -scrollY * 0.005; // Adjust the movement speed as needed
+  // Update model rotation based on mouse movement
+  model.rotation.y += (targetRotationY - model.rotation.y) * 0.05;
 
+  // Update camera position based on scroll
+  var cameraY = -targetCameraY;
   camera.position.setY(cameraY);
   camera.lookAt(model.position);
 
@@ -186,6 +211,7 @@ function animate() {
 
 // Start the animation loop
 animate();
+
 
 
     },
