71ee24426d3961638bbde40811d7bc2a0b9bcef8
MRPH95 <134096222+MRPH95@users.noreply.github.com>
2023-05-22 14:09:11 -0400

    Update SLherothreejs.js

--- SLherothreejs.js
+++ SLherothreejs.js
@@ -10,7 +10,7 @@ function init() {
     0.1,
     1000
   );
-  camera.position.set(0, .25, 1); // Adjust the camera position as needed
+  camera.position.set(0, 0, 1); // Adjust the camera position as needed
 
   // Create a WebGL renderer
   var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -154,7 +154,7 @@ function init() {
       
 // Constants
 var MOUSE_SENSITIVITY = 0.003; // Adjust the mouse sensitivity as needed
-var TARGET_OFFSET_Y = 0.05; // Offset in the y-axis
+var TARGET_OFFSET_Y = 0; // Offset in the y-axis
 var MAX_ROTATION_X = Math.PI / 12; // Maximum rotation in radians
 var SPIN_SENSITIVITY = 0.005; // Adjust the spin sensitivity as needed
 
